a8abed05e052802605194aeeb5fdd1d81d105d10
Rebecca Paz <r.pazyaquian@gmail.com>
2014-05-26 16:13:05 -0400

    key capturing, subroutine stack
    Added (basic) key capturing capability and a subroutine stack. The key
    capturing must be extended to tell whether a key is pressed or
    released, and a timer to measure system time between operations must be
    added.

--- chip8.py
+++ chip8.py
@@ -36,19 +36,29 @@ class Chip8:
 		# Currently pressed keys. I might need to think a little more about actually getting this into play...
 		self.keys = []
 
-		# # Tkinter window.
+		# Tkinter window.
 		self.tile_res = tile_res
 		self.root = Tk()
+
+		# Subroutine stack. An array of 16 shorts (32 bytes) that will hold a bunch of addresses, I assume.
+		# Also the stack pointer, which points to the current position of the stack.
+		self.stack = [0]*16
+		self.sp = 0x000
 
 	# How this should work:
 	# 1. initialize a 4096 large RAM array.
 	# 2. Read the input program two bytes at a time.
 	# 3. These two bytes must be compared against a list of pre-defined opcodes. This must be performed with bitmasking (right?).
 
+	def key(self, event):
+		print "pressed {0}".format(repr(event.char))
+
 	def load_screen(self):
 
 		self.w = Canvas(self.root, width = (64*self.tile_res), height = (32*self.tile_res))
+		self.w.bind('<Key>', self.key)
 		self.w.pack()
+		self.w.focus_set()
 
 	def update_screen(self):
 
@@ -126,7 +136,7 @@ class Chip8:
 		while True:
 
 			print self.command_to_opcode(self.short_to_command(self.shortify(self.ram[self.pc:self.pc+2])))
-			# time.sleep(1)
+			time.sleep(1)
 
 	def shortify(self, byte_pair):
 		short = (byte_pair[0] << 8 | byte_pair[1])
@@ -136,7 +146,6 @@ class Chip8:
 		command = (((short >> 12) & 0xF), ((short >> 8) & 0x0F), ((short >> 4) & 0x00F), ((short >> 0) & 0x000F), short)
 		return command
 
-	# The following is taken from http://devernay.free.fr/hacks/chip8/chip8def.htm:
 	def command_to_opcode(self, command):  # command should be a tuple from short_to_command()
 
 		op = command[0]  # integer "position" 1, i.e. the first "digit"
@@ -148,9 +157,11 @@ class Chip8:
 		self.pc += 2
 		
 		if op == 0:
-			if ((a == 0 and b == 15) and c == 0):
+			if ((a == 0 and b == 14) and c == 0):
 				return 'Clear the screen.'
-			elif ((a == 0 and b == 15) and c == 15):
+			elif ((a == 0 and b == 14) and c == 14):
+				self.sp -= 1
+				self.pc = self.stack[self.sp]
 				return 'Return from subroutine.'
 			else:
 				return '0{0:x}{1:x}{2:x} - Call RCA-1802 program from 0x{0:x}{1:x}{2:x}. (this is the original interpreter)'.format(a, b, c)
@@ -163,6 +174,10 @@ class Chip8:
 				self.pc = insn & 0xFFF
 			return '1{0:x}{1:x}{2:x} - Jumps to address 0x{0:x}{1:x}{2:x}.'.format(a, b, c)
 		elif op == 2:
+			self.stack[self.sp] = self.pc
+			self.sp += 1
+			self.pc = insn & 0xFFF
+			print self.stack
 			return '2{0:x}{1:x}{2:x} - Calls subroutine at 0x{0:x}{1:x}{2:x}.'.format(a, b, c)
 		elif op == 3:
 			if self.v[a] == (insn & 0xFF):
@@ -228,7 +243,7 @@ class Chip8:
 					self.v[15] = 1
 					self.v[b] = (self.v[b] - self.v[a]) & 0xFF
 				return '8{0:x}{1:x}7 - Sets V{0:x} to (V{1:x} minus V{0:x}). VF is set to 0 when there\'s a borrow, and 1 when there isn\'t.'.format(a, b)
-			elif c == 15:
+			elif c == 14:
 				self.v[15] = (self.v[a] & 1)
 				self.v[a] = (self.v[a] << 1)
 				return '8{0:x}{1:x}E - Shifts V{0:x} left by one. VF is set to the value of the most significant bit of V{0:x} before the shift.'.format(a, b)
@@ -254,43 +269,52 @@ class Chip8:
 		elif op == 14:
 			# The basic idea is to record a set of currently pressed keys, and add a key to the set when it's pressed - remove when it's released.
 			# This will be a little easier in Javascript...remember, this is just a prototype.
-			if (b == 9 and c == 15):
+			if (b == 9 and c == 14):
 				key_codes = {0: 0, 1:1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15}
 				key = raw_input('Enter a key (0, 1, 2, a, b, f, etc.): ')  # temporary solution until i can get keylogging going
 				if key_codes[key] == self.v[a]:
 					self.pc += 2
-				return 'E{0:x}9E - Skips the next instruction if the key stored in V{0:x} is pressed.'.format(hex(a)[-1])
+				return 'E{0:x}9E - Skips the next instruction if the key stored in V{0:x} is pressed.'.format(a)
 			elif (b == 10 and c == 1):
 				key_codes = {0: 0, 1:1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15}
 				key = raw_input('Enter a key (0, 1, 2, a, b, f, etc.): ')  # temporary solution until i can get keylogging going
 				if key_codes[key] != self.v[a]:
 					self.pc += 2
-				return 'E{0:x}A1 - Skips the next instruction if the key stored in V{0:x} isn\'t pressed.'.format(hex(a)[-1])
+				return 'E{0:x}A1 - Skips the next instruction if the key stored in V{0:x} isn\'t pressed.'.format(a)
 			else:
 				return 'invalid opcode'
 		elif op == 15:
 			if (b == 0 and c == 7):
 				# TODO: Understand and create delay timer.
-				return 'F{0:x}07 - Set V{0:x} to value of delay timer.'.format(hex(a)[-1])
+				return 'F{0:x}07 - Set V{0:x} to value of delay timer.'.format(a)
 			elif (b == 0 and c == 10):
 				key_codes = {0: 0, 1:1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15}
 				key = raw_input('Enter a key (0, 1, 2, a, b, f, etc.): ')  # temporary solution until i can get keylogging going
 				self.v[a] = key_codes[key]
-				return 'F{0:x}0A - A key press is awaited, and then stored in V{0:x}.'.format(hex(a)[-1])
+				return 'F{0:x}0A - A key press is awaited, and then stored in V{0:x}.'.format(a)
 			elif (b == 1 and c == 5):
 				self.delay = a
-				return 'F{0:x}15 - Set delay timer to V{0:x}.'.format(hex(a)[-1])
+				return 'F{0:x}15 - Set delay timer to V{0:x}.'.format(a)
 			elif (b == 1 and c == 8):
 				self.sound = a
-				return 'F{0:x}18 - Set sound timer to V{0:x}.'.format(hex(a)[-1])
-			elif (b == 1 and c == 15):
+				return 'F{0:x}18 - Set sound timer to V{0:x}.'.format(a)
+			elif (b == 1 and c == 14):
 				self.i += self.v[a]
-				return 'F{0:x}1E - Add V{0:x} to I.'.format(hex(a)[-1])
+				return 'F{0:x}1E - Add V{0:x} to I.'.format(a)
 			elif (b == 2 and c == 9):
 				# RAM locations are from [(5 * key value (e.g. 0, 2, 10)):_+5]
-				return 'F{0:x}29 - Set I to the location of the sprite for the character in V{0:x}. Characters 0-F (in hexadecimal) are represented by a 4x5 font.'.format(hex(a)[-1])
+				# e.g. self.ram[self.v[a] * 5], with VX = 0, will drop you off at self.ram[0] and read from there to self.ram[4].
+				# '0' font data is located from self.ram[0] to self.ram[4].
+				return 'F{0:x}29 - Set I to the location of the sprite for the character in V{0:x}. Characters 0-F (in hexadecimal) are represented by a 4x5 font.'.format(a)
 			elif (b == 3 and c == 3):
-				return 'F{0:x}33 - Store the Binary-coded decimal representation of V{0:x}, with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2. (In other words, take the decimal representation of V{0:x}, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.)'.format(hex(a)[-1])
+				ones = (self.v[a] / 1) % 10
+				tens = (self.v[a] / 10) % 10
+				hunds = (self.v[a] / 100) % 10
+
+				self.ram[self.i] = hunds
+				self.ram[self.i + 1] = tens
+				self.ram[self.i + 2] = ones
+				return 'F{0:x}33 - Store the Binary-coded decimal representation of V{0:x}, with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2. (In other words, take the decimal representation of V{0:x}, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.)'.format(a)
 				# that last one looks like a huge pain in the ass...
 			elif (b == 5 and c == 5):
 				v_stores = self.v[0:a]
@@ -298,13 +322,13 @@ class Chip8:
 				for v in v_stores:
 					self.ram[store_address] = v
 					store_address += 1
-				return 'F{0:x}55 - Store V0 to V{0:x} in memory starting at address I.'.format(hex(a)[-1])
+				return 'F{0:x}55 - Store V0 to V{0:x} in memory starting at address I.'.format(a)
 			elif (b == 6 and c == 5):
 				read_address = self.i
 				for v in self.v[0:a]:
 					v = self.ram[read_address]
 					read_address += 1
-				return 'F{0:x}65 - Fill V0 to V{0:x} with values from memory starting at address I.'.format(hex(a)[-1])
+				return 'F{0:x}65 - Fill V0 to V{0:x} with values from memory starting at address I.'.format(a)
 			else:
 				return 'invalid opcode'
 		else:
@@ -312,14 +336,14 @@ class Chip8:
 
 # This is a CHIP-8 program:
 
-program = [0xA2, 0x1E, 0xC2, 0x01, 0x32, 0x01, 0xA2, 0x1A, 0xD0,
-0x14, 0x70, 0x04, 0x30, 0x40, 0x12, 0x00, 0x60, 0x00, 0x71, 0x04,
-0x31, 0x20, 0x12, 0x00, 0x12, 0x18, 0x80, 0x40, 0x20, 0x10, 0x20,
-0x40, 0x80, 0x10]
+# program = [0xA2, 0x1E, 0xC2, 0x01, 0x32, 0x01, 0xA2, 0x1A, 0xD0,
+# 0x14, 0x70, 0x04, 0x30, 0x40, 0x12, 0x00, 0x60, 0x00, 0x71, 0x04,
+# 0x31, 0x20, 0x12, 0x00, 0x12, 0x18, 0x80, 0x40, 0x20, 0x10, 0x20,
+# 0x40, 0x80, 0x10]
 
 # This is a typical command:
 
-command = 0x1200  # Jump to address 0x200.
+# command = 0x1200  # Jump to address 0x200.
 
 # How this should work:
 # 1. initialize a 4096 large bit array.
@@ -329,6 +353,10 @@ command = 0x1200  # Jump to address 0x200.
 # Testing
 
 if __name__ == '__main__':
+
+	file = open(sys.argv[1], 'rb')
+	program = bytearray(file.read())
+	file.close()
 
 	chip = Chip8(20)
 
