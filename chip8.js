abb91c022755b6d1a6b5f74d5d5abbaa48822f63
Rebecca Paz <rebecca@kyn.is>
2014-06-20 15:02:40 -0400

    add frame skip
    Added a variable for how many opcodes are run per frame. Will
    eventually allow me to have the user decide the number.

--- chip8.js
+++ chip8.js
@@ -762,6 +762,8 @@ canvas.setAttribute("width", width.toString());
 
 program = [0xa2, 0x1e, 0xc2, 0x01, 0x32, 0x01, 0xa2, 0x1a, 0xd0, 0x14, 0x70, 0x04, 0x30, 0x40, 0x12, 0x00, 0x60, 0x00, 0x71, 0x04, 0x31, 0x20, 0x12, 0x00, 0x12, 0x18, 0x80, 0x40, 0x20, 0x10, 0x20, 0x40, 0x80, 0x10];
 
+frame_skip = 11;
+
 off_color = "#000000";
 on_color = "#FFFFFF";
 
@@ -898,7 +900,7 @@ function frame_loop() {
     // console.log('running loop');
 
     if (!waiting_for_key) {
-        for (var i = 0; i < 10; i++) {  // run ten opcodes   
+        for (var i = 0; i < frame_skip; i++) {  // run ten opcodes   
             run_opcode();
             if (waiting_for_key) break;
         }
